132ed5c3561ea2ecd6293dec68c03264a0db9dc2
awarren4 <ajw3pq@virginia.edu>
2015-02-10 20:09:39 -0500

    Exercise 5 - Editing code from assignment 4

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -1,67 +1,71 @@
-# store file name.
+#Import collections package
+import collections
+
+#store the romeo and juliet file name.
 file_name = "romeoandjuliet.txt"
 
-#create variables to store number of lines, words, and character
+#create empty variables that will be used to count the number of lines and words in the file
 line_counter = 0
-character_with_spaces_counter = 0
 word_counter = 0
-character_counter = 0
 
-#create an empty list to count occurances of words
-empty_list = [] 
+#create empty variable that will be used to count the number of characters, excluding spaces between words, in the file. 
+character_with_no_spaces_counter = 0
 
-# open the file.
-my_file = open( "romeoandjuliet.txt", "r")
+#create empty variable that will be used to count the number of characters, including spaces between words, in the file. 
+character_with_spaces_counter = 0
 
-# loop over lines in file.
+#create an empty list that will be used to store every word in the file
+list_of_all_words_in_file = [] 
+
+#open the file.
+my_file = open( "romeoandjuliet.txt", "r" )
+
+
+#loop over lines in file
 for current_line in my_file:
 
-    # print the line
+
+    #print the current line - this will show which lines have been processed in the document
     '''print( "current_line:" + current_line )'''
 
-    #increment counter for each line read and print variable value
+    #count the number of lines in the file - increment line_counter for each line read 
     line_counter = line_counter + 1
-    '''print( "incremented line_counter:" + str(line_counter) )'''
 
-    #count the number of characters in each line with spaces included, increment counter, print value
+    #Remove the extra blank space from the line 
     stripped_string = current_line.strip()
-    character_with_spaces_counter = character_with_spaces_counter + len(stripped_string)
-    '''print( "current stripped word:" + str(stripped_string) )
-    print( "individual character length for line:" + str( len( stripped_string) ) )
-    print( "incremented character counter:" + str(character_with_spaces_counter) )'''
 
-    #split each line/string into individual words and print number of words
-    word_list = current_line.split()
-    word_counter = word_counter + int(len( word_list )  )
-    ''' print( "line converted to list of words:" + str(word_list) )
-    print( "individual string length:" + str( len( word_list ) ) )
-    print( "incremented word counter:" + str(word_counter) )'''
-
-    #new loop - split each word into characters and print number of characters - doesn't include spaces
-    for current_word in word_list: 
-        '''print( "current word:" + str(current_word) )
-        print( "individual character length:" + str( len(current_word) ) )'''
-        character_counter = character_counter + len(current_word)
-
-    #catalog - create a list with all words in the file - start with lines that have extra blank space removed
+    #split the line into individual words that are stored in a list 
     word_stripped_list = stripped_string.split()
-    empty_list = empty_list + word_stripped_list  
+
+    #increment list_of_all_words_in_file with current list of words
+    list_of_all_words_in_file = list_of_all_words_in_file + word_stripped_list  
+
+    #count the number of characters in the line, with spaces included, then use this value to increment character_with_spaces_counter
+    character_with_spaces_counter = character_with_spaces_counter + len( stripped_string )
+
+    #count the number of words in the list then use this value to increment word_counter
+    word_counter = word_counter + len( word_stripped_list )
+    
+
+    #new loop over words in line 
+    for current_word in word_stripped_list: 
+
+
+        #count the number of characters in the word then use this value to increment character_with_no_spaces_counter - this excludes spaces between words
+        character_with_no_spaces_counter = character_with_no_spaces_counter + len( current_word )
+
  	
-#-- END loop over words and lines of file --#
+#end loop over words and lines of file
 my_file.close()
 
-#Check list
-'''print("view list of all words in file:" + str(empty_list))
-print("number of words in whole file:" + str(len(empty_list)))'''
 
-#Final Counter Values 
-print( "final line counter:" + str(line_counter) )
-print( "final word counter:" + str(word_counter) )
-print( "final character counter not including spaces:" + str(character_counter) )
-print( "final character counter including spaces:" + str(character_with_spaces_counter) )
+#print the final values for number of lines, words, and characters
+print( "final line counter:" + str( line_counter) )
+print( "final word counter:" + str( word_counter) )
+print( "final character counter not including spaces:" + str( character_with_no_spaces_counter) )
+print( "final character counter including spaces:" + str( character_with_spaces_counter) )
 
-#Final Catalog
-import collections
-print ("catalog:")
-print collections.Counter(empty_list)
+#print the final catalog - create a dictionary that takes each unique word in the list and counts the number of occurances 
+print ( "catalog:" )
+print collections.Counter( list_of_all_words_in_file )
 
